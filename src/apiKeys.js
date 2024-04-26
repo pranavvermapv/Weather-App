73c2fc621128d774d8d790812b3dd76da2b106e9
Pranav verma <70197674+pranavvermapv@users.noreply.github.com>
2024-04-26 15:13:04 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "bfcb7fc9e3efaf04d697e12cf22771ff",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
